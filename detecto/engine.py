7cbb53eb4c87be38147b7c8793b8ad28983d9698
cmiao <cmiao@huskers.unl.edu>
2020-11-12 19:26:23 -0600

    update

--- detecto/engine.py
+++ detecto/engine.py
@@ -3,13 +3,11 @@ import sys
 import time
 import torch
 import torchvision
-
 import J2ME.detecto.utils as utils
-from J2ME.detecto.coco_utils import get_coco_api_from_dataset
 from J2ME.detecto.coco_eval import CocoEvaluator
+from J2ME.detecto.coco_utils import get_coco_api_from_dataset
 
 def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
-
     model.train()
     metric_logger = utils.MetricLogger(delimiter="  ")
     metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
@@ -19,26 +17,19 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
     if epoch == 0:
         warmup_factor = 1. / 1000
         warmup_iters = min(1000, len(data_loader) - 1)
-
         lr_scheduler = utils.warmup_lr_scheduler(optimizer, warmup_iters, warmup_factor)
 
     for images, targets, _ in metric_logger.log_every(data_loader, print_freq, header):
         images = list(image.to(device) for image in images)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-
         loss_dict = model(images, targets)
-
         losses = sum(loss for loss in loss_dict.values())
-
         # reduce losses over all GPUs for logging purposes
         loss_dict_reduced = utils.reduce_dict(loss_dict)
         losses_reduced = sum(loss for loss in loss_dict_reduced.values())
-
         loss_value = losses_reduced.item()
-
         if not math.isfinite(loss_value):
             print("Loss is {}, stopping training".format(loss_value))
-            print(loss_dict_reduced)
             sys.exit(1)
 
         optimizer.zero_grad()
@@ -47,11 +38,9 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
 
         if lr_scheduler is not None:
             lr_scheduler.step()
-
         metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
-
-    return metric_logger
+    return loss_dict_reduced, losses_reduced
 
 
 def _get_iou_types(model):
@@ -70,20 +59,21 @@ def _get_iou_types(model):
 def evaluate(model, data_loader, device):
     n_threads = torch.get_num_threads()
     # FIXME remove this and make paste_masks_in_image run on the GPU
-    torch.set_num_threads(n_threads)
+    torch.set_num_threads(1)
     model.eval()
     metric_logger = utils.MetricLogger(delimiter="  ")
     header = 'Test:'
 
     coco = get_coco_api_from_dataset(data_loader.dataset)
     iou_types = _get_iou_types(model)
+    print('iou types: %s'%iou_types)
     coco_evaluator = CocoEvaluator(coco, iou_types)
 
     for images, targets, _ in metric_logger.log_every(data_loader, 100, header):
         images = list(img.to(device) for img in images)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
-        torch.cuda.synchronize()
+        #torch.cuda.synchronize()
         model_time = time.time()
         outputs = model(images)
 
--- detecto/fasterRCNN.py
+++ detecto/fasterRCNN.py
@@ -10,7 +10,6 @@ import os
 import sys
 import time
 import datetime
-import logging
 import numpy as np
 import pandas as pd
 from pathlib import Path
@@ -45,54 +44,40 @@ def get_transform(train):
         transforms.append(T.RandomHorizontalFlip(0.5))
     return T.Compose(transforms)
 
-def get_model(num_classes=3):
-    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True, progress=True)
-    in_features = model.roi_heads.box_predictor.cls_score.in_features
-    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-    return model
-'''    
-def get_model(num_classes, backbone='resnet50',
-                s=((30),) , ar=((1.0),),
-                box_nms_thresh = 0.3):
-    anchor_generator = AnchorGenerator(sizes=s, aspect_ratios=ar)
-
+def get_model(num_classes, backbone='resnet50'):
     if backbone == 'resnet50':
-        model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True, progress=True, 
-                                                        rpn_anchor_generator=anchor_generator,
-                                                        box_nms_thresh = box_nms_thresh)
+        model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+
     elif backbone == 'resnet101':
         from J2ME.detecto.models import fasterrcnn_resnet101_fpn
-        model = fasterrcnn_resnet101_fpn(pretrained=True, progress=True,
-                                        rpn_anchor_generator=anchor_generator,
-                                        box_nms_thresh = box_nms_thresh)
+        model = fasterrcnn_resnet101_fpn(pretrained=True)
+
     else:
         sys.exit('only resnet50 and resnet101 supported!')
 
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
     return model
-'''
+
 def train(args):
     """
-    %prog train train_csv, train_dir, valid_csv, valid_dir, model_name_prefix
+    %prog train train_csv, train_dir, valid_csv, valid_dir, mn_prefix
     Args:
         train_csv: csv file containing training image 'fn' and 'targets'
         train_dir: directory where training images are located
         valid_csv: csv file containing validation image 'fn' and 'targets'
         valid_dir: directory where validation images are located
-        model_name_prefix: the prefix of the output model name 
+        mn_prefix: the prefix of the output model name 
     """
     p = OptionParser(train.__doc__)
-    p.add_option('--batchsize', default=2, type='int', 
+    p.add_option('--batchsize', default=20, type='int', 
                     help='batch size')
-    p.add_option('--epoch', default=26, type='int', 
+    p.add_option('--epoch', default=200, type='int', 
                     help='number of total epochs')
-    p.add_option('--patience', default=20, type='int', 
+    p.add_option('--patience', default=10, type='int', 
                     help='patience in early stopping')
-    p.add_option('--backbone', default='resnet50',
+    p.add_option('--backbone', default='resnet50', choices=('resnet50', 'resnet101'),
                     help='pretrained model name used as backbone')
-    p.add_option('--output_dir', default='.',
-                    help='directory path where to save')
     p.add_option('--disable_slurm', default=False, action="store_true",
                  help='run directly without generating slurm job')
     p.add_slurm_opts(job_prefix=train.__name__)
@@ -100,30 +85,22 @@ def train(args):
     opts, args = p.parse_args(args)
     if len(args) != 5:
         sys.exit(not p.print_help())
-    train_csv, train_dir, valid_csv, valid_dir, model_name_prefix = args
+    train_csv, train_dir, valid_csv, valid_dir, mn_prefix = args
 
     # genearte slurm file
     if not opts.disable_slurm:
-        cmd_header = 'ml singularity'
-        cmd = "singularity exec docker://unlhcc/pytorch:1.5.0 "\
-            "python3 -m schnablelab.CNN.TransLearning prediction "\
-            f"{saved_model} {test_csv} {test_dir} {output} "\
-            f"--batchsize {opts.batchsize} --disable_slurm "
-        if opts.pretrained_mn:
-         cmd += f"--pretrained_mn {opts.pretrained_mn}"
+        cmd = "python -m J2ME.detecto.fasterRCNN train "\
+            f"{train_csv} {train_dir} {valid_csv} {valid_dir} {mn_prefix} "\
+            f"--batchsize {opts.batchsize} "\
+            f"--epoch {opts.epoch} "\
+            f"--patience {opts.patience} "\
+            f"--backbone {opts.backbone} --disable_slurm "
         put2slurm_dict = vars(opts)
-        put2slurm_dict['cmd_header'] = cmd_header
         put2slurm([cmd], put2slurm_dict)
         sys.exit()
 
-
-    logfile = model_name_prefix + '.log'
-    #histfile = model_name_prefix + '.hist.csv'
-    logging.basicConfig(filename=logfile, level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(message)s")
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    logging.debug('device: %s'%device)
-    logging.debug('pytorch version: %s'%torch.__version__)
-    logging.debug('cuda version: %s'%torch.version.cuda)
+    print('device: %s\npytorch version: %s\ncuda version: %s'%(device, torch.__version__, torch.version.cuda))
 
     train_dataset = ObjectDetectionDataset(train_csv, train_dir, get_transform(train=True))
     valid_dataset = ObjectDetectionDataset(valid_csv, valid_dir, get_transform(train=False))
@@ -131,44 +108,41 @@ def train(args):
     valid_loader = DataLoader(valid_dataset, batch_size=opts.batchsize, collate_fn=utils.collate_fn)
     dataloaders_dict = {'train': train_loader, 'valid': valid_loader}
 
-    model = get_model(3) # either leaf tip or background
+    model = get_model(num_classes=3, backbone=opts.backbone) # healthy leaf tip, cut leaf tip, background
     model.to(device)
-
     params = [p for p in model.parameters() if p.requires_grad] # 'requires_grad' default is Ture (will be trained)
-    optimizer = torch.optim.SGD(params, lr=0.005,
-                                momentum=0.9, weight_decay=0.0005)
-    # and a learning rate scheduler
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
-                                                   step_size=3,
-                                                   gamma=0.1)
-    print('start training')
+    optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
+    # learning rate scheduler
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+    print('start training...')
     start_time = time.time()
+
+    early_stopping = EarlyStopping(mn_prefix, patience=opts.patience, verbose=True, delta=0.01)
     for epoch in range(opts.epoch):
-        # train for one epoch, printing every 10 iterations
-        train_one_epoch(model, optimizer, dataloaders_dict['train'], device, epoch, print_freq=10)
+        loss_dict, total_loss = train_one_epoch(model, optimizer, dataloaders_dict['train'], device, epoch, print_freq=1)
+        print('sum of losses: %s'%total_loss)
+        print('loss dict:\n', loss_dict)
         # update the learning rate
         lr_scheduler.step()
-
-        # save model
-        utils.save_on_master({
-                'model': model.state_dict(),
-                'optimizer': optimizer.state_dict(),
-                'lr_scheduler': lr_scheduler.state_dict(),
-                'args': args,
-                'opts': opts,
-                'epoch': epoch},
-                os.path.join(opts.output_dir, f'{model_name_prefix}_{epoch}.pth'))
-
         # evaluate on the test dataset
-        evaluate(model, dataloaders_dict['valid'], device=device)
-    
+        coco_evaluator = evaluate(model, dataloaders_dict['valid'], device=device)
+        ap = coco_evaluator.coco_eval['bbox'].stats[0]
+        #ious = coco_evaluator.coco_eval['bbox'].ious
+        print('average AP: %s'%ap)
+        valid_loss = 1-ap
+        early_stopping(valid_loss, model)
+        if early_stopping.early_stop:
+            print('Early stopping')
+            break
+        print('Best average precision: {:0.3f}'.format(1-early_stopping.val_loss_min))
+
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
     print('Training time {}'.format(total_time_str))
 
 def predict(args):
     """
-    %prog predict saved_model test_csv, test_dir, output
+    %prog predict saved_model test_csv, test_dir, output_prefix
     Args:
         saved_model: saved model with either a .pt or .pth file extension
         test_csv: csv file (comma separated without header) containing all testing image filenames
@@ -176,47 +150,50 @@ def predict(args):
         output: csv file saving prediction results
     """
     p = OptionParser(predict.__doc__)
-    p.add_option('--backbone', default='resnet50',
+    p.add_option('--backbone', default='resnet50', choices=('resnet50','resnet101'),
                     help='pretrained model name used as backbone')
-    p.add_option('--score_cutoff', type='float', default=0.9,
+    p.add_option('--score_cutoff', type='float', default=0.7,
                     help='set score cutoff')
+    p.add_option('--show_box', default=False, action="store_true",
+                 help = 'generate image with predicted bounding box.')
+    p.add_option('--output_dir', default='.',
+                 help = 'specify the output directory for saving prediction results.')
     p.add_option('--disable_slurm', default=False, action="store_true",
                  help='run directly without generating slurm job')
-    p.add_option('--show_box', default=False, action="store_true",
-                 help = 'generate image with bounding box.')
     p.add_slurm_opts(job_prefix=predict.__name__)
 
     opts, args = p.parse_args(args)
     if len(args) != 4:
         sys.exit(not p.print_help())
-    saved_model, test_csv, test_dir, output = args
+    saved_model, test_csv, test_dir, output_prefix = args
+
+    out_dir = Path(opts.output_dir)
+    if not out_dir.exists():
+        print('output dir %s does not exist, created.'%out_dir)
+        out_dir.mkdir()
 
     if not opts.disable_slurm:
-        cmd_header = 'ml singularity'
-        cmd = "singularity exec docker://unlhcc/pytorch:1.5.0 "\
-            "python3 -m schnablelab.CNN.TransLearning prediction "\
-            f"{saved_model} {test_csv} {test_dir} {output} "\
-            f"--batchsize {opts.batchsize} --disable_slurm "
-        if opts.pretrained_mn:
-         cmd += f"--pretrained_mn {opts.pretrained_mn}"
+        cmd = "python -m J2ME.detecto.fasterRCNN predict "\
+            f"{saved_model} {test_csv} {test_dir} {output_prefix} "\
+            f"--backbone {opts.backbone} "\
+            f"--score_cutoff {opts.score_cutoff} "\
+            f"--output_dir {opts.output_dir} "\
+            f"--show_box {opts.show_box} --disable_slurm "
         put2slurm_dict = vars(opts)
-        put2slurm_dict['cmd_header'] = cmd_header
         put2slurm([cmd], put2slurm_dict)
         sys.exit()
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(f'device detected: {device}')
-    model = get_model(num_classes=3)
-    checkpoint = torch.load(saved_model, map_location={'cuda:0': 'cpu'})
-
-    model.load_state_dict(checkpoint['model'])
-    model = model.to(device)
+    model = get_model(num_classes=3, backbone=opts.backbone)
+    model.load_state_dict(torch.load(saved_model, map_location=device))
     model.eval()
 
     test_dataset = ObjectDetectionDataset(test_csv, test_dir, get_transform(train=False), only_image=True, sep=',')
     test_loader = DataLoader(test_dataset, batch_size=1)
 
-    filenames, boxes, labels, scores, lcs = [], [], [], [], []
+    filenames, boxes, labels, scores, lcs = [], [], [], [], [] # lcs: leaf counts
+    print('start testing...')
     for imgs, _, fns in test_loader:
         imgs = imgs.to(device)
         results = model(imgs)
@@ -231,12 +208,12 @@ def predict(args):
         if opts.show_box:
             img = show_box(Path(test_dir)/fn, boxes[idxs], labels[idxs], scores[idxs])
             img_out_fn = fn.replace('.png', '.prd.jpg') if fn.endswith('.png') else fn.replace('.jpg', '.prd.jpg')
-            img.save(img_out_fn)
+            img.save(out_dir/img_out_fn)
 
         filenames.append(fn)
         lcs.append(len(idxs))
-    pd.DataFrame(dict(zip(['fn', 'lc'], [filenames, lcs]))).to_csv('%s.prediction.csv'%output, index=False)
-
+    pd.DataFrame(dict(zip(['fn', 'lc'], [filenames, lcs]))).to_csv(out_dir/('%s.prediction.csv'%output_prefix), index=False)
+    print('Done! check leaf counting results in %s.prediction.csv'%output_prefix)
 
 if __name__ == "__main__":
     main()
